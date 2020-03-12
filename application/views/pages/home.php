0dc240a05528ba2b2623378742ead1c067a9f06c
Laurensius Joshua Anrico Agustinus <joshua.anrico30@gmail.com>
2020-03-12 11:36:10 +0700

    update tampilan awal

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -10,7 +10,7 @@
         <div class="col-md-2 col-sm-2">
           <div class="service-info">
             <div class="icon">
-              <img src="<?php echo base_url(); ?>assets/img/layanan1.png">
+              <img src="<?php echo base_url(); ?>assets/img/layanan1.png" class="img-responsive" style="width: 140px; margin-top: -16px;">
             </div>
             <div class="icon-info">
               <h4>Pemeriksaan Gigi</h4>
@@ -20,7 +20,7 @@
         <div class="col-md-2 col-sm-2">
           <div class="service-info">
             <div class="icon">
-              <img src="<?php echo base_url(); ?>assets/img/layanan2.png">
+              <img src="<?php echo base_url(); ?>assets/img/layanan2.png" class="img-responsive" style="width: 140px; margin-top: -16px;">
             </div>
             <div class="icon-info">
               <h4>Pengobatan Gigi</h4>
@@ -30,17 +30,17 @@
         <div class="col-md-2 col-sm-2">
           <div class="service-info">
             <div class="icon">
-              <img src="<?php echo base_url(); ?>assets/img/layanan3.png">
+              <img src="<?php echo base_url(); ?>assets/img/layanan3.png" class="img-responsive" style="width: 140px; margin-top: -16px;">
             </div>
             <div class="icon-info">
               <h4>Edukasi Gigi</h4>
             </div>
           </div>
-		</div>
+    </div>
         <div class="col-md-2 col-sm-2">
           <div class="service-info">
             <div class="icon">
-              <img src="<?php echo base_url(); ?>assets/img/layanan4.png">
+              <img src="<?php echo base_url(); ?>assets/img/layanan4.png" class="img-responsive" style="width: 140px; margin-top: -16px;">
             </div>
             <div class="icon-info">
               <h4>Pencegahan Kerusakan Gigi</h4>
@@ -50,7 +50,7 @@
         <div class="col-md-2 col-sm-2">
           <div class="service-info">
             <div class="icon">
-              <img src="<?php echo base_url(); ?>assets/img/layanan5.png">
+              <img src="<?php echo base_url(); ?>assets/img/layanan5.png" class="img-responsive" style="width: 140px; margin-top: -16px;">
             </div>
             <div class="icon-info">
               <h4>Mengembalikan Fungsi Gigi</h4>
@@ -66,7 +66,7 @@
   <section id="cta-1" class="section-padding">
     <div class="container">
       <div class="row">
-      	<div class="col-md-12">
+        <div class="col-md-12">
           <h2 class="ser-title">Artikel Kesehatan Gigi</h2>
           <hr class="botm-line">
         </div>
@@ -86,9 +86,8 @@
               <a href="#" class="medi-info-btn">READ MORE</a>
             </div>
           </div>
-          <div class="col-md-4 col-sm-4 mt-boxy-3">
-            <div class="mt-boxy-color"></div>
-            <div class="end-info">
+          <div class="col-md-4 col-sm-4">
+            <div class="medi-info">
               <h3>Judul Artikel</h3>
               <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
               <a href="#" class="medi-info-btn">READ MORE</a>
