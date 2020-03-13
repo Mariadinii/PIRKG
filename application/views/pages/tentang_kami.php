4c9627062d4f04e63beca81efda4682473b3ffe8
M. Hafidh Dliyaul Haq <mhafidhdliyaulh11@gmail.com>
2020-03-13 16:33:35 +0700

    Update tentang_kami.php
    ADD tampilan halaman web tentang kami

--- application/views/pages/tentang_kami.php
+++ application/views/pages/tentang_kami.php
@@ -1,1 +1,34 @@
-<h2><?= $title ?></h2>
+<!--tentang kami-->
+<section id="about" class="section-padding">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4 col-sm-6 col-xs-12">
+        <div class="section-title">
+          <h2 class="head-title lg-line">Tentang Kami</h2>
+          <hr class="botm-line">
+          <p class="sec-para">PIRKG merupakan website informasi dan reservasi kesehatan gigi Klinik Drg. Norman Fajeri. PIRKG hadir untuk memberikan wawasan mengenai pentingnya menjaga kesehatan gigi dan mulut.</p>
+        </div>
+      </div>
+      <div class="col-md-8 col-sm-6 col-xs-12">
+        <div style="visibility: visible;" class="col-sm-9 more-features-box">
+          <div class="more-features-box-text">
+            <div class="more-features-box-text-icon"> <i class="fa fa-angle-right" aria-hidden="true"></i> </div>
+            <div class="more-features-box-text-description">
+              <h3>Visi</h3>
+              <p>Menjadi klinik dokter gigi yang profesional serta mampu memberikan edukasi tentang pentingnya kesehatan gigi.</p>
+            </div>
+          </div>
+          <div class="more-features-box-text">
+            <div class="more-features-box-text-icon"> <i class="fa fa-angle-right" aria-hidden="true"></i> </div>
+            <div class="more-features-box-text-description">
+              <h3>Misi</h3>
+              <p><i class="fa fa-circle"></i> Memberikan pengetahuan tentang kesehatan gigi.</p>
+              <p><i class="fa fa-circle"></i> Memberikan pelayanan kesehatan gigi.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<!--/ tentang kami-->
