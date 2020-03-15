ba1cf7dce87f1e585a39c1a89ff5acd1fc00e657
M. Hafidh Dliyaul Haq <mhafidhdliyaulh11@gmail.com>
2020-03-15 21:48:01 +0700

    Edit style.css, home.php, dan Add tentang_kami.php
    Melakukan perubahan pada file style.css dan home.php serta penambahan kode program pada tentang_kami.php

--- application/views/pages/tentang_kami.php
+++ application/views/pages/tentang_kami.php
@@ -32,3 +32,54 @@
   </div>
 </section>
 <!--/ tentang kami-->
+<!--contact-->
+<section id="contact" class="section-padding">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <h2 class="ser-title">Hubungi Kami</h2>
+          <hr class="botm-line">
+      </div>
+      <div class="col-md-4 col-sm-4">
+        <h3>Contact Info</h3>
+        <div class="space"></div>
+        <p><i class="fa fa-map-marker fa-fw pull-left fa-2x"></i>Jalan Kiwi No. 18<br> Kedaton, Bandar Lampung, 35147</p>
+        <div class="space"></div>
+        <p><i class="fa fa-clock-o fa-fw pull-left fa-2x"></i>Setiap hari kerja<br> Pukul 16.00 - selesai</p>
+        <div class="space"></div>
+        <p><i class="fa fa-phone fa-fw pull-left fa-2x"></i>0813-8xxx-xxxx</p>
+      </div>
+      <div class="col-md-8 col-sm-8 marb20">
+        <div class="contact-info">
+          <h3 class="cnt-ttl">Mempunyai pertanyaan seputar gigi?</h3>
+          <div class="space"></div>
+          <div id="sendmessage">Your message has been sent. Thank you!</div>
+          <div id="errormessage"></div>
+          <form action="" method="post" role="form" class="contactForm">
+            <div class="form-group">
+              <input type="text" name="name" class="form-control br-radius-zero" id="name" placeholder="Nama" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+              <div class="validation"></div>
+            </div>
+            <div class="form-group">
+              <input type="email" class="form-control br-radius-zero" name="email" id="email" placeholder="Email" data-rule="email" data-msg="Please enter a valid email" />
+              <div class="validation"></div>
+            </div>
+            <div class="form-group">
+              <input type="text" class="form-control br-radius-zero" name="subject" id="subject" placeholder="Subjek" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" />
+              <div class="validation"></div>
+            </div>
+            <div class="form-group">
+              <textarea class="form-control br-radius-zero" name="message" rows="5" data-rule="required" data-msg="Please write something for us" placeholder="Message"></textarea>
+              <div class="validation"></div>
+            </div>
+
+            <div class="form-action">
+              <button type="submit" class="btn btn-form">Kirim</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<!--/ contact-->
